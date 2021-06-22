007415ad96ecdca3614af56f0aa9b0f938a5dc40
Dominic Kavanagh <dsk1354@g.rit.edu>
2021-06-22 14:46:27 -0400

    Created baseline for the BoardView class.

--- src/main/java/com/webcheckers/board/BoardView.java
+++ src/main/java/com/webcheckers/board/BoardView.java
@@ -1,4 +1,12 @@
 package com.webcheckers.board;
 
+import java.util.Iterator;
+
 public class BoardView {
+
+    /* Not currently implemented
+    public Iterator<Row>iterator(){
+        return;
+    }
+    */
 }
