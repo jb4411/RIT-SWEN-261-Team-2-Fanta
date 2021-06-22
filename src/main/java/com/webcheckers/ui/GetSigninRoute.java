f3b09ad0b3b75abe7ecf18293f94895bd3ff90c0
jb4411 <66132069+jb4411@users.noreply.github.com>
2021-06-22 14:55:17 -0400

    Added the handle() method to GetSigninRoute.java.

--- src/main/java/com/webcheckers/ui/GetSigninRoute.java
+++ src/main/java/com/webcheckers/ui/GetSigninRoute.java
@@ -1,7 +1,9 @@
 package com.webcheckers.ui;
 
-import spark.TemplateEngine;
+import spark.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.logging.Logger;
 
@@ -10,7 +12,7 @@ import java.util.logging.Logger;
  *
  * @author Jesse Burdick-Pless jb4411@g.rit.edu
  */
-public class GetSigninRoute {
+public class GetSigninRoute implements Route {
     private static final Logger LOG = Logger.getLogger(GetSigninRoute.class.getName());
 
     private final TemplateEngine templateEngine;
@@ -23,5 +25,20 @@ public class GetSigninRoute {
     public GetSigninRoute(final TemplateEngine templateEngine) {
         this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
         LOG.config("GetSigninRoute is initialized.");
+    }
+
+    /**
+     * Render the WebCheckers Signin page.
+     *
+     * @param request the HTTP request
+     * @param response the HTTP response
+     * @return the rendered HTML for the Home page
+     */
+    @Override
+    public Object handle(Request request, Response response) {
+        LOG.finer("GetSigninRoute is invoked.");
+        Map<String, Object> vm = new HashMap<>();
+        // render the View
+        return templateEngine.render(new ModelAndView(vm , "signin.ftl"));
     }
 }
