57e1e064864fa26fb6984494194c3c88a93cfe48
jb4411 <66132069+jb4411@users.noreply.github.com>
2021-06-22 14:45:49 -0400

    Added a constructor to GetSigninRoute.java.

--- src/main/java/com/webcheckers/ui/GetSigninRoute.java
+++ src/main/java/com/webcheckers/ui/GetSigninRoute.java
@@ -1,9 +1,22 @@
 package com.webcheckers.ui;
 
+import spark.TemplateEngine;
+
+import java.util.Objects;
+import java.util.logging.Logger;
+
 /**
  * The UI Controller to GET the Signin page.
  *
  * @author Jesse Burdick-Pless jb4411@g.rit.edu
  */
 public class GetSigninRoute {
+    private static final Logger LOG = Logger.getLogger(GetSigninRoute.class.getName());
+
+    private final TemplateEngine templateEngine;
+
+    public GetSigninRoute(final TemplateEngine templateEngine) {
+        this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
+        LOG.config("GetSigninRoute is initialized.");
+    }
 }
