fd3a79af330d34f9b51bf58be0b87c472619319f
Dominic Kavanagh <dsk1354@g.rit.edu>
2021-06-24 12:40:11 -0400

    Adds comments for the methods describing their purpose and added some functionality to isValid.

--- src/main/java/com/webcheckers/board/Space.java
+++ src/main/java/com/webcheckers/board/Space.java
@@ -3,15 +3,33 @@ package com.webcheckers.board;
 public class Space {
     private int cellIndx;
 
-    public int getCellIndx() {
+    /**
+     * Not currently implemented
+     * @return The index of this space within the row.
+     */
+    public int getCellIdx(){
         return cellIndx;
     }
 
+    /**
+     * Not currently fully implemented
+     * Checks whether this space is a valid space to move a piece
+     * @return Java Iterator of the Spaces within a single row.
+     */
     public boolean isValid(){
-        return true;
+        if (getPiece() != null){
+            return false;
+        }
+        else return true;
     }
-
+    /**
+     * Not currently implemented
+     * Creates a Java Iterator of the Spaces within a single row.
+     * @return the piece if there is a piece, otherwise null.
+     */
     public Piece getPiece(){
         return null;
     }
+
+
 }
