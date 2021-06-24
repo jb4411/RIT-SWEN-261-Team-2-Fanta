0bd3933892ea2ee9f36584a0df4024be7b932668
Eric Landers <esl7511@gmail.com>
2021-06-24 12:53:31 -0400

    changing around the names of the classes so it makes more sense

--- src/main/java/com/webcheckers/board/BoardView.java
+++ src/main/java/com/webcheckers/board/BoardView.java
@@ -1,19 +1,31 @@
 package com.webcheckers.board;
 
 import java.util.Iterator;
+import java.util.List;
+import java.util.LinkedList;
 
-import javax.swing.text.PlainDocument;
+public class BoardView {
+    private Player player1;
+    private Player player2;
+    private List<Row> board;
 
-public class BoardView implements Iterable<Row>{
-
-    private GameView game;
-
-    public BoardView(Player player1, PLayer player2){
-        this.game = new GameView(player1, player2);
+    public BoardView(Player player1, Player player2){
+        this.player1 = player1;
+        this.player2 = player2;
+        this.board = new LinkedList<>();
+        initBoard();
     }
 
-    @Override
-    public Iterator<Row> iterator(){
-        return game.getBoard().iterator();
+    public List<Row> getBoard(){
+        return board;
+    }
+
+    public void initBoard(){
+        boolean valid = true;
+
+        for(int i=0; i<8; i++){
+            board.add(new Row());
+            valid = !valid;
+        }
     }
 }
