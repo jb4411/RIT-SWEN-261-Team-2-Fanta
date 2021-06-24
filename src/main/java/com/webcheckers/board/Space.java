19dca2322ef8b2d94f307fc1effb608d2c38a7b6
Jesse Zhao <jlz6146@g.rit,edu>
2021-06-24 12:36:18 -0400

    Added initial implementation of public functions (J Zhao)

--- src/main/java/com/webcheckers/board/Space.java
+++ src/main/java/com/webcheckers/board/Space.java
@@ -1,4 +1,17 @@
 package com.webcheckers.board;
 
 public class Space {
+    private int cellIndx;
+
+    public int getCellIndx() {
+        return cellIndx;
+    }
+
+    public boolean isValid(){
+        return true;
+    }
+
+    public Piece getPiece(){
+        return null;
+    }
 }
