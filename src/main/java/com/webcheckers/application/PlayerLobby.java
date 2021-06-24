afe6c4a58bc4b2645c6c52bad164dd40135197a3
jb4411 <66132069+jb4411@users.noreply.github.com>
2021-06-24 11:34:30 -0400

    Worked on addPlayer in PlayerLobby.java

--- src/main/java/com/webcheckers/application/PlayerLobby.java
+++ src/main/java/com/webcheckers/application/PlayerLobby.java
@@ -1,17 +1,31 @@
 package com.webcheckers.application;
 
+import com.webcheckers.model.Player;
+
+import java.util.HashMap;
+
 /**
  * Holds all currently logged in players and is used to add more players as they log in.
  *
  * @author Jesse Burdick-Pless jb4411@g.rit.edu
  */
 public class PlayerLobby {
+    private final HashMap<String, Player> players;
+
+    public PlayerLobby() {
+        players = new HashMap<>();
+    }
 
     public synchronized boolean addPlayer(String name) {
         if(!name.matches("^[a-zA-Z\\s]*$")) {
             return false;
+        } else if(players.containsKey(name)) {
+            return false;
         }
 
+        Player newPlayer = new Player(name);
+        players.put(name, newPlayer);
+        return true;
     }
 
 
