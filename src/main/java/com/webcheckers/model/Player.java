5828d94ebf75882c9a49be6fb237b9c4a85c91fe
Eric Landers <esl7511@gmail.com>
2021-06-24 11:30:27 -0400

    Adding hashcode and equals method to Player class

--- src/main/java/com/webcheckers/model/Player.java
+++ src/main/java/com/webcheckers/model/Player.java
@@ -2,12 +2,33 @@ package com.webcheckers.model;
 
 public class Player{
     private String name;
+    private String sessionID;
 
-    public Player(String name){
+    public Player(String name, String sessionID){
         this.name = name;
+        this.sessionID = sessionID;
     }
 
     public String getName(){
         return name;
+    }
+
+    public String getSessionID(){
+        return sessionID;
+    }
+    @Override
+    public boolean equals(Object object){
+        if(object == this){
+            return true;
+        }
+        if(!(obj instanceof Player)){
+            return false;
+        }
+        final Player that = (Player) object;
+        return this.name.equals(that.name);
+    }
+    @Override
+    public int hashCode(){
+        return name.hashCode();
     }
 }
