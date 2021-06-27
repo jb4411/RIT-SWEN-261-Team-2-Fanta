2f680e2a4a92c86d688e2987eaaccd43becb3360
jb4411 <66132069+jb4411@users.noreply.github.com>
2021-06-27 15:25:31 -0400

    Added documentation to Row.java.

--- src/main/java/com/webcheckers/board/Row.java
+++ src/main/java/com/webcheckers/board/Row.java
@@ -3,17 +3,29 @@ package com.webcheckers.board;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * A class to represent a row of the checkerboard.
+ *
+ * @author Jesse Burdick-Pless jb4411@g.rit.edu
+ */
 public class Row implements Iterable<Space> {
     private int index;
     private List<Space> spaces;
 
+    /**
+     * Create a new Row.
+     *
+     * @param index this row's index
+     * @param spaces this row's list of spaces
+     */
     public Row(int index, List<Space> spaces) {
         this.index = index;
         this.spaces = spaces;
     }
 
     /**
-     * Getter for the index of the row withint he board.
+     * Getter for the index of the row within he board.
+     *
      * @return The index of this row within the board.
      */
     public int getIndex(){
@@ -21,8 +33,8 @@ public class Row implements Iterable<Space> {
     }
 
     /**
-     * Not currently implemented
      * Creates a Java Iterator of the Spaces within a single row.
+     *
      * @return Java Iterator of the Spaces within a single row.
      */
     public Iterator<Space>iterator(){
