635cad2d90cf34cfeb80695fd73a86c93c330af4
jb4411 <66132069+jb4411@users.noreply.github.com>
2021-06-27 15:23:57 -0400

    Added documentation to Piece.java.

--- src/main/java/com/webcheckers/board/Piece.java
+++ src/main/java/com/webcheckers/board/Piece.java
@@ -1,5 +1,10 @@
 package com.webcheckers.board;
 
+/**
+ * A abstract class to represent a checker piece.
+ *
+ * @author Jesse Burdick-Pless jb4411@g.rit.edu
+ */
 public abstract class Piece {
     private Type rank;
     private Color color;
@@ -14,18 +19,39 @@ public abstract class Piece {
         WHITE
     }
 
+    /**
+     * An abstract definition of a copy constructor to create a copy of this piece.
+     *
+     * @return a copy of this piece
+     */
     public abstract Piece getCopy();
 
+    /**
+     * Create a new piece. Used as a super constructor by King.java and Single.java.
+     *
+     * @param rank the type of piece this piece should be
+     * @param color the color this piece should be
+     */
     public Piece(Type rank, Color color){
         this.rank = rank;
         this.color = color;
     }
 
+    /**
+     * Return what type of piece this piece is.
+     *
+     * @return this pieces type
+     */
     public Type getType(){
         return rank;
 
     }
 
+    /**
+     * Return what color this piece is.
+     *
+     * @return this pieces color
+     */
     public Color getColor(){
         return color;
     }
