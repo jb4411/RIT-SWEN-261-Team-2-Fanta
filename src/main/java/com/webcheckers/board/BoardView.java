177f972c8f27d5ff47354e640f30c31990e52c9c
jb4411 <66132069+jb4411@users.noreply.github.com>
2021-06-27 14:33:54 -0400

    Added documentation to BoardView.java.

--- src/main/java/com/webcheckers/board/BoardView.java
+++ src/main/java/com/webcheckers/board/BoardView.java
@@ -4,8 +4,11 @@ import com.webcheckers.model.Player;
 
 import java.util.*;
 
-import com.webcheckers.model.Player;
-
+/**
+ * A class to represent the game board.
+ *
+ * @author Jesse Burdick-Pless jb4411@g.rit.edu
+ */
 public class BoardView implements Iterable<Row>{
     private Player red;
     private Player white;
@@ -14,7 +17,12 @@ public class BoardView implements Iterable<Row>{
     private static final int NUM_ROWS = 8;
     private static final int NUM_COLS = 8;
 
-
+    /**
+     * Creates a new game board.
+     *
+     * @param red the player using red pieces
+     * @param white the player using white pieces
+     */
     public BoardView(Player red, Player white){
         this.red = red;
         this.white = white;
@@ -22,19 +30,38 @@ public class BoardView implements Iterable<Row>{
         initBoard();
     }
 
+    /**
+     * A copy constructor that can also return a flipped copy of the board.
+     *
+     * @param board the board to be copied
+     * @param flip whether or not the board should be flipped
+     */
     public BoardView(BoardView board, boolean flip) {
         this.board = new Space[NUM_ROWS][NUM_COLS];
         for(int row = 0; row < NUM_ROWS; row++) {
             for (int col = 0; col < NUM_COLS; col++) {
-                this.board[row][col] = new Space(board.getRow(NUM_ROWS - row - 1)[NUM_COLS - col - 1]);
+                if(flip) {
+                    this.board[row][col] = new Space(board.getRow(NUM_ROWS - row - 1)[NUM_COLS - col - 1]);
+                } else {
+                    this.board[row][col] = new Space(board.getRow(row)[col]);
+                }
             }
         }
     }
 
+    /**
+     * Returns the row at the index given.
+     *
+     * @param rowIdx the index of the desired row
+     * @return the row at the given index
+     */
     private Space[] getRow(int rowIdx) {
         return board[rowIdx];
     }
 
+    /**
+     * Initialize the board to a valid initial board state.
+     */
     public void initBoard() {
         for(int row = 0; row < NUM_ROWS; row++) {
             for(int col = 0; col < NUM_COLS; col++) {
@@ -53,6 +80,11 @@ public class BoardView implements Iterable<Row>{
         }
     }
 
+    /**
+     * Return an iterator of the board.
+     *
+     * @return a linked list of the rows in the board
+     */
     @Override
     public Iterator<Row> iterator(){
         Collection<Row> lst = new LinkedList<>();
