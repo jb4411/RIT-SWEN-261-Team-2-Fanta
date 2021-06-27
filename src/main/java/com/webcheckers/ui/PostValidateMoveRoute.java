35b65174c0b634244d7c7912caf5b01de1652288
jb4411 <66132069+jb4411@users.noreply.github.com>
2021-06-27 15:09:30 -0400

    Added documentation to PostValidateMoveRoute.java.

--- src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
+++ src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
@@ -7,16 +7,33 @@ import spark.TemplateEngine;
 
 import java.util.logging.Logger;
 
+/**
+ * The UI Controller to POST validating a move.
+ *
+ * @author Jesse Burdick-Pless jb4411@g.rit.edu
+ */
 public class PostValidateMoveRoute implements Route  {
     private static final Logger LOG = Logger.getLogger(PostSigninRoute.class.getName());
 
     private final TemplateEngine templateEngine;
 
+    /**
+     * Create the Spark Route (UI controller) to handle all {@code POST /validateMove} HTTP requests.
+     *
+     * @param templateEngine the HTML template rendering engine
+     */
     public PostValidateMoveRoute(TemplateEngine templateEngine) {
         LOG.finer("PostValidateMoveRoute is invoked.");
         this.templateEngine = templateEngine;
     }
 
+    /**
+     * Check if the move made is valid.
+     *
+     * @param request the HTTP request
+     * @param response the HTTP response
+     * @return the rendered HTML for the game page
+     */
     @Override
     public Object handle(Request request, Response response) throws Exception {
         return null;
