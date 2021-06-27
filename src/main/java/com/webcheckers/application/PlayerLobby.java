b55d2a5f0cd949bfbdaf805b8f4598a1e6d515ff
jb4411 <66132069+jb4411@users.noreply.github.com>
2021-06-27 14:52:17 -0400

    Added documentation to PlayerLobby.java.

--- src/main/java/com/webcheckers/application/PlayerLobby.java
+++ src/main/java/com/webcheckers/application/PlayerLobby.java
@@ -13,12 +13,21 @@ import java.util.Set;
 public class PlayerLobby {
     private final HashMap<String, Player> players;
 
+    public enum NameStatus {INVALID, VALID, DUPLICATE}
+
+    /**
+     * Create a new PlayerLobby to hold active players.
+     */
     public PlayerLobby() {
         this.players = new HashMap<>();
     }
 
-    public enum NameStatus {INVALID, VALID, DUPLICATE}
-
+    /**
+     * Try to add a new player, returns a message to the user if the selected name is already in use.
+     *
+     * @param name the name of the player to try to add
+     * @return the status of the player creation
+     */
     public synchronized NameStatus addPlayer(String name) {
         if(!name.matches("[a-zA-Z]+[a-zA-Z\\s]*$")) {
             return NameStatus.INVALID;
@@ -31,14 +40,30 @@ public class PlayerLobby {
         return NameStatus.VALID;
     }
 
+    /**
+     * Return the number of currently active players.
+     *
+     * @return the number of active players
+     */
     public int getNumPlayers() {
         return this.players.size();
     }
 
+    /**
+     * Return the player with the name given, if a player with that name exists.
+     *
+     * @param name the name of the player to get
+     * @return the player with the given name, if a player with that name exists
+     */
     public Player getPlayer(String name){
         return players.get(name);
     }
 
+    /**
+     * Return a set of the names of all currently active players.
+     *
+     * @return a set of the names of all currently active players
+     */
     public Set<String> getAllPlayers() {
         return players.keySet();
     }
