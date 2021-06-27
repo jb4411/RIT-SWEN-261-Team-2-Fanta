2952303e6b95e44e05ff999814e9d2f8b6e6bc1f
jb4411 <66132069+jb4411@users.noreply.github.com>
2021-06-27 15:30:22 -0400

    Added documentation to Space.java.

--- src/main/java/com/webcheckers/board/Space.java
+++ src/main/java/com/webcheckers/board/Space.java
@@ -1,16 +1,33 @@
 package com.webcheckers.board;
 
+/**
+ * A class to represent a space on the checkerboard.
+ *
+ * @author Jesse Burdick-Pless jb4411@g.rit.edu
+ */
 public class Space {
     private int cellIdx;
     private Piece piece;
     private boolean valid;
 
+    /**
+     * Create a new Space.
+     *
+     * @param cellIdx the index of this space in its row
+     * @param piece the piece on this square, if there is one
+     * @param valid if this square is a valid square to move to
+     */
     public Space(int cellIdx, Piece piece, boolean valid) {
         this.cellIdx = cellIdx;
         this.piece = piece;
         this.valid = valid;
     }
 
+    /**
+     * A copy constructor to create a copy of this space.
+     *
+     * @param space the space to be copied
+     */
     public Space(Space space) {
         this.cellIdx = space.getCellIdx();
         if(space.getPiece() != null) {
@@ -22,7 +39,8 @@ public class Space {
     }
 
     /**
-     * Not currently implemented
+     * Return the index of this space in its row.
+     *
      * @return The index of this space within the row.
      */
     public int getCellIdx(){
@@ -30,9 +48,9 @@ public class Space {
     }
 
     /**
-     * Not currently fully implemented
-     * Checks whether this space is a valid space to move a piece
-     * @return Java Iterator of the Spaces within a single row.
+     * Checks whether this space is a valid space to move a piece to.
+     *
+     * @return whether or not a piece can be put on this square
      */
     public boolean isValid(){
         if(!this.valid){
@@ -41,8 +59,10 @@ public class Space {
             return this.piece == null;
         }
     }
+
     /**
-     * Not currently implemented
+     * Return the piece on this square, if there is one.
+     *
      * @return the piece if there is a piece, otherwise null.
      */
     public Piece getPiece(){
