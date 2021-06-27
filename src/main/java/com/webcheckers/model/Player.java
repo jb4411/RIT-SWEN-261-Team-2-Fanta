b6c4a1eda61005498060ec7ef1563a1511bf90c3
jb4411 <66132069+jb4411@users.noreply.github.com>
2021-06-27 15:05:01 -0400

    Added documentation to Player.java.

--- src/main/java/com/webcheckers/model/Player.java
+++ src/main/java/com/webcheckers/model/Player.java
@@ -1,10 +1,11 @@
 package com.webcheckers.model;
+
 /**
  * Player class: holds all player information.
  *
  * @author Eric Landers esl7511@rit.edu
+ * @author Jesse Burdick-Pless jb4411@g.rit.edu
  */
-
 public class Player{
     private String name;
     private Color color;
@@ -14,22 +15,48 @@ public class Player{
         WHITE
     }
 
+    /**
+     * Return the color of pieces this player is using.
+     *
+     * @return the color of pieces this player is using
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     * Set this player's piece color.
+     *
+     * @param color the color this player should be using
+     */
     public void setColor(Color color) {
         this.color = color;
     }
 
+    /**
+     * Set this player's name.
+     *
+     * @param name this player's new name
+     */
     public Player(String name){
         this.name = name;
     }
 
+    /**
+     * Return this player's name.
+     *
+     * @return this player's name
+     */
     public String getName(){
         return name;
     }
 
+    /**
+     * Check if this player is equal to the object passed in.
+     *
+     * @param object the object this player is being compared to
+     * @return whether or not they are equal
+     */
     @Override
     public boolean equals(Object object){
         if(object == this){
@@ -41,6 +68,12 @@ public class Player{
         final Player that = (Player) object;
         return this.name.equals(that.name);
     }
+
+    /**
+     * Return the hashcode of this player.
+     *
+     * @return this player's hashcode
+     */
     @Override
     public int hashCode(){
         return name.hashCode();
