a0efedd6d8ef63b22dc523287044c999727f328a
jb4411 <66132069+jb4411@users.noreply.github.com>
2021-06-27 15:16:17 -0400

    Added documentation to King.java.

--- src/main/java/com/webcheckers/board/King.java
+++ src/main/java/com/webcheckers/board/King.java
@@ -1,15 +1,28 @@
 package com.webcheckers.board;
 
-
+/**
+ * A class to represent a king piece.
+ *
+ * @author Jesse Burdick-Pless jb4411@g.rit.edu
+ */
 public class King extends Piece {
+    /**
+     * Create a new king piece.
+     *
+     * @param color the color this piece should be
+     */
+    public King(Color color) {
+        super(Type.KING, color);
+    }
+
+    /**
+     * A copy constructor to create a copy of this piece.
+     *
+     * @return a copy of this piece
+     */
     @Override
     public Piece getCopy() {
-        return new King(this.getType(), this.getColor());
+        return new King(this.getColor());
     }
-
-    public King(Type rank, Color color) {
-        super(rank, color);
-    }
-
 
 }
