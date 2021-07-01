235c88901026f009e8963d780844cab40d7d0d69
Jesse Zhao <jlz6146@g.rit,edu>
2021-07-01 14:03:36 -0400

    Added initial implementation of GetSignOutRoute (J Zhao)

--- src/main/java/com/webcheckers/ui/GetSignOutRoute.java
+++ src/main/java/com/webcheckers/ui/GetSignOutRoute.java
@@ -1,8 +1,29 @@
 package com.webcheckers.ui;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.logging.Logger;
 
 public class GetSignOutRoute implements Route {
     private static final Logger LOG = Logger.getLogger(GetSigninRoute.class.getName());
     private final TemplateEngine templateEngine;
+
+    public GetSignOutRoute(final TemplateEngine templateEngine){
+        this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
+        LOG.config("GetSignOutRoute is initialized");
+    }
+
+    /**
+     * Signs the player out
+     *
+     * @param request
+     * @param response
+     * @return
+     */
+    @Override
+    public Object handle(Request request, Response response){
+        LOG.finer("GetSignOutRoute is invoked.");
+        Map<String, Object> vm = new HashMap<>();
+    }
 }
