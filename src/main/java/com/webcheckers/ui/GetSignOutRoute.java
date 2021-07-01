e941500afe32b0ec554e56ec2da856120a57afc6
Jesse Zhao <jlz6146@g.rit,edu>
2021-07-01 13:53:25 -0400

    Added initial implementation of GetSignOutRoute (J Zhao)

---
+++ src/main/java/com/webcheckers/ui/GetSignOutRoute.java
@@ -0,0 +1,8 @@
+package com.webcheckers.ui;
+
+import java.util.logging.Logger;
+
+public class GetSignOutRoute implements Route {
+    private static final Logger LOG = Logger.getLogger(GetSigninRoute.class.getName());
+    private final TemplateEngine templateEngine;
+}
