d71987ec15a82990b88429bc4d50141d53e4cfbd
jb4411 <66132069+jb4411@users.noreply.github.com>
2021-07-07 18:13:46 -0400

    Created new feature branch, and added name to Move.java.

--- src/main/java/com/webcheckers/model/Move.java
+++ src/main/java/com/webcheckers/model/Move.java
@@ -7,6 +7,7 @@ import com.google.gson.Gson;
  * A class to represent Move object.
  *
  * @author Eric Landers esl7511@rit.edu
+ * @author Jesse Burdick-Pless jb4411@g.rit.edu
  */
 public class Move{
 
