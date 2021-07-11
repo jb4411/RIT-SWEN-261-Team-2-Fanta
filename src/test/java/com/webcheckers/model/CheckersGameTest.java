6ada18f733016ec459e62661ea4b021a852fcc06
jb4411 <66132069+jb4411@users.noreply.github.com>
2021-07-11 17:28:57 -0400

    Created CheckersGameTest.java, updated BoardViewTest.java slightly.

---
+++ src/test/java/com/webcheckers/model/CheckersGameTest.java
@@ -0,0 +1,14 @@
+package com.webcheckers.model;
+
+import org.junit.jupiter.api.Tag;
+
+/**
+ * The unit test suite for the {@link CheckersGame} component.
+ *
+ * @author Jesse Burdick-Pless jb4411@g.rit.edu
+ */
+@Tag("Model-tier")
+public class CheckersGameTest {
+
+
+}
